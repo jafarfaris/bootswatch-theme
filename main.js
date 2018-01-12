314389d65b758999c7bfc063f0170e6c91bdd170
JafarFaris <jafarfaris42@gmail.com>
2018-01-12 12:57:13 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// Changing themes bootswatch
+function changeBg(x){
+    var link = document.getElementById('bootswatch');
+    link.href = 'https://bootswatch.com/4/' + x.value + '/bootstrap.min.css';
+}
+
+
